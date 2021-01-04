6fa3bdad57c103234c459659366ff5c55a636330
amolnilwarn@gmail.com <amolnilwarn@gmail.com>
2021-01-04 12:44:18 +0530

    initial commit for unit test

---
+++ src/com/calc/test/StringCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.calc.test;
+
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+class StringCalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
