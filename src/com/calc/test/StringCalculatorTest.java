abfcc0f6dd59b352e0979b5588fb9f12a15f1517
amolnilwarn@gmail.com <amolnilwarn@gmail.com>
2021-01-04 13:03:32 +0530

    unit test for test new line

--- src/com/calc/test/StringCalculatorTest.java
+++ src/com/calc/test/StringCalculatorTest.java
@@ -29,5 +29,10 @@ class StringCalculatorTest {
     	assertEquals(6, StringCalculator.add("1,2,3"));
     }
 	
+	@Test
+    public void testNewLine(){
+    	assertEquals(6, StringCalculator.add("1\n2,3"));
+    }
+	
 
 }
